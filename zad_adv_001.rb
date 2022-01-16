afd33cfc9edfd1cc99d8a3c21c74830b6a7eabd8
sieki <kciupak@o2.pl>
2022-01-16 21:29:23 +0100

    zadanie1 cd

--- zad_adv_001.rb
+++ zad_adv_001.rb
@@ -3,41 +3,52 @@ if __FILE__ == $0
   i = 0
   while i<5 do
 
-    puts (" Type filename with extension .txt (or type EXIT if you want to quit) end press ENTER :")
-    input = gets.to_s
+    puts (' Type filename with extension .txt (or type EXIT if you want to quit) end press ENTER :')
+    input = $stdin.gets # .to_s # roznica gets, STDIN.gets $stdin.gets
 
     break if input == "exit\n"
 
-    puts (input)
+    # puts (input)
 
-    file1 = File.open(input), "r"
-    numbers = []
+    file1 = File.open(input, 'r+')   # ("/home/code/git001/test/#{input}", 'w')
+    # puts file1
+
+    numbers = [0,1]
 
     File.foreach(input) do |line|
-      numbers << line
+      numbers << 9  # line
+      puts ("#{line}")
     end
 
-    puts (numbers.to_a)
+    puts ("#{numbers}")
 
     break if file1 != nil
+
       puts ("Can't open or No file for typed name.")
 
 
     # puts ("Liczby z pliku")
-    # file.each.with_index do |line|
+    # file1.each.with_index do |line|
     # puts ("#{line}")
     # end
 
     # File.foreach(file1).with_index do |line, line_num|
-      # puts "#{line_num}: #{line}"
+    #   puts "#{line_num}: #{line}"
     # end
 
 
-    # for line in file                        # iteracja po liniach pliku
-    # puts line                             # wyswietla kazda linie
-    # end
-    i+=1
+
+    i += 1
   end
+
+   puts ("Liczby z pliku")
+    file1.each.with_index do |line|
+    puts ("#{line}")
+    end
+
+    for line in file1                        # iteracja po liniach pliku
+      puts line                             # wyswietla kazda linie
+    end
 
   file1.close
 
