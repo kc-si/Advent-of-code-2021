3ab1c3c0fccb6f4c1d3f0db3551978c9b136ea2a
sieki <kciupak@o2.pl>
2022-01-22 11:42:16 +0100

    task_002a.rb final modify

--- read_file.rb
+++ read_file.rb
@@ -1,7 +1,26 @@
-def read_file
+def read_file_old
   puts(' Type filename with extension if exist (or type EXIT if you want to quit) end press ENTER :')
   input_file = gets
   input_file = input_file.chop
+
+  return if input_file == "exit"
+
+  if File.readable? (input_file)
+    input_data = File.read(input_file)
+  else
+      puts('Wrong filename or file not exist.')
+  end
+end
+
+def read_file
+
+  if ARGV[0]
+    input_file = ARGV[0]
+  else
+    puts(' Type filename with extension if exist (or type EXIT if you want to quit) end press ENTER :')
+    input_file = gets
+    input_file = input_file.chop
+  end
 
   return if input_file == "exit"
 
