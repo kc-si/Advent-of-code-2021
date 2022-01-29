5dc000d4e273aa7fe57e9b3f7662c9593d58fc98
sieki <kciupak@o2.pl>
2022-01-29 14:56:48 +0100

    task_004.rb version1.0 with spec readya

---
+++ spec/task_004a_spec.rb
@@ -0,0 +1,85 @@
+require './lib/task_004a.rb'
+
+RSpec.describe 'metod parse_input(input_data) - input data processing:' do
+  it ' short input_data to puzzle_input' do
+    input_data = "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1\n\n22 13 17 11  0\n
+    8  2 23  4 24\n21  9 14 16  7\n 6 10  3 18  5\n 1 12 20 15 19\n\n 3 15  0  2 22\n 9 18 13 17  5\n
+    19  8  7 25 23\n20 11 10 24  4\n14 21 16 12  6\n\n14 21 17 24  4\n10 16 15  9 19\n18  8 23 26 20\n
+    22 11 13  6  5\n 2  0 12  3  7\n"
+
+    puzzle_input = parse_input(input_data)
+
+    expect(puzzle_input).to eq([[7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 13, 6, 15, 25, 12, 22,
+    18, 20, 8, 19, 3, 26, 1],
+    [[[22, 13, 17, 11, 0], [8, 2, 23, 4, 24], [21, 9, 14, 16, 7], [6, 10, 3, 18, 5], [1, 12, 20, 15, 19]],
+    [[3, 15, 0, 2, 22], [9, 18, 13, 17, 5], [19, 8, 7, 25, 23], [20, 11, 10, 24, 4], [14, 21, 16, 12, 6]],
+    [[14, 21, 17, 24, 4], [10, 16, 15, 9, 19], [18, 8, 23, 26, 20], [22, 11, 13, 6, 5], [2, 0, 12, 3, 7]]]])
+  end
+end
+
+RSpec.describe 'metod check_board(numbers, board) :' do
+  puzzle_input = [[7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 13, 6, 15, 25, 12, 22, 18, 20, 8, 19, 3, 26, 1],
+  [[[22, 13, 17, 11, 0], [8, 2, 23, 4, 24], [21, 9, 14, 16, 7], [6, 10, 3, 18, 5], [1, 12, 20, 15, 19]],
+  [[3, 15, 0, 2, 22], [9, 18, 13, 17, 5], [19, 8, 7, 25, 23], [20, 11, 10, 24, 4], [14, 21, 16, 12, 6]],
+  [[14, 21, 17, 24, 4], [10, 16, 15, 9, 19], [18, 8, 23, 26, 20], [22, 11, 13, 6, 5], [2, 0, 12, 3, 7]]]]
+
+  it ' checking board (board wins)' do
+    numbers_drawn = 11
+    board_number = 2
+
+    check = check_board(puzzle_input[0][0..numbers_drawn], puzzle_input[1][board_number])
+
+    expect(check).to eq(1)
+  end
+
+  it ' checking board (board not wins)' do
+    numbers_drawn = 11
+    board_number = 1
+
+    check = check_board( puzzle_input[0][0..numbers_drawn], puzzle_input[1][board_number] )
+
+    expect(check).to eq(nil)
+  end
+end
+
+RSpec.describe 'metod find_winners(puzzle_input) :' do
+  puzzle_input = [[7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 13, 6, 15, 25, 12, 22, 18, 20, 8, 19, 3, 26, 1],
+  [[[22, 13, 17, 11, 0], [8, 2, 23, 4, 24], [21, 9, 14, 16, 7], [6, 10, 3, 18, 5], [1, 12, 20, 15, 19]],
+  [[3, 15, 0, 2, 22], [9, 18, 13, 17, 5], [19, 8, 7, 25, 23], [20, 11, 10, 24, 4], [14, 21, 16, 12, 6]],
+  [[14, 21, 17, 24, 4], [10, 16, 15, 9, 19], [18, 8, 23, 26, 20], [22, 11, 13, 6, 5], [2, 0, 12, 3, 7]]]]
+
+  it ' finding winner board ' do
+    results = find_winners(puzzle_input)
+
+    expect(true).to eq(results[:winners] == [2, 0, 1] && results[:numbers_drawn] == 14)
+  end
+end
+
+RSpec.describe ' metod sum_unmarked_numbers(puzzle_input, results) :' do
+  puzzle_input = [[7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 13, 6, 15, 25, 12, 22, 18, 20, 8, 19, 3, 26, 1],
+  [[[22, 13, 17, 11, 0], [8, 2, 23, 4, 24], [21, 9, 14, 16, 7], [6, 10, 3, 18, 5], [1, 12, 20, 15, 19]],
+  [[3, 15, 0, 2, 22], [9, 18, 13, 17, 5], [19, 8, 7, 25, 23], [20, 11, 10, 24, 4], [14, 21, 16, 12, 6]],
+  [[14, 21, 17, 24, 4], [10, 16, 15, 9, 19], [18, 8, 23, 26, 20], [22, 11, 13, 6, 5], [2, 0, 12, 3, 7]]]]
+
+  it ' count sum of not drown numbers' do
+    results = {winners: [2, 0, 1], numbers_drawn: 14}
+
+    sum = sum_unmarked_numbers(puzzle_input, results)
+
+    expect(sum).to eq(148)
+  end
+end
+
+
+RSpec.describe ' metod  calculate_answer(puzzle_input) :' do
+  puzzle_input = [[7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 13, 6, 15, 25, 12, 22, 18, 20, 8, 19, 3, 26, 1],
+  [[[22, 13, 17, 11, 0], [8, 2, 23, 4, 24], [21, 9, 14, 16, 7], [6, 10, 3, 18, 5], [1, 12, 20, 15, 19]],
+  [[3, 15, 0, 2, 22], [9, 18, 13, 17, 5], [19, 8, 7, 25, 23], [20, 11, 10, 24, 4], [14, 21, 16, 12, 6]],
+  [[14, 21, 17, 24, 4], [10, 16, 15, 9, 19], [18, 8, 23, 26, 20], [22, 11, 13, 6, 5], [2, 0, 12, 3, 7]]]]
+
+  it ' calculate task answer' do
+    answer = calculate_answer(puzzle_input)
+
+    expect(answer).to eq(1924)
+  end
+end
